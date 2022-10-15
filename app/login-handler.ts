1e7d2895d3ed24251fc716518a647888b92b076b
sumiren <oota.software@gmail.com>
2022-10-15 17:49:25 +0900

    fix: status code

--- app/login-handler.ts
+++ app/login-handler.ts
@@ -1,4 +1,4 @@
-import fastify, { FastifyPluginAsync, FastifyRequest, FastifyReply } from 'fastify'
+import fastify, {FastifyPluginAsync, FastifyRequest, FastifyReply} from 'fastify'
 
 
 export const loginHandler: FastifyPluginAsync = (fastify) => {
@@ -6,19 +6,25 @@ export const loginHandler: FastifyPluginAsync = (fastify) => {
   return Promise.resolve();
 }
 
-export const handler = async (request: FastifyRequest<{Querystring: IQuerystring}>) => {
-  validateStatically(request)
-  return { message: "you've logged in!" };
+export const handler = async (request: FastifyRequest<{ Querystring: IQuerystring }>, reply: FastifyReply) => {
+  const result = validateStatically(request)
+  if (!result.noProblem) {
+    reply.code(result.statusCode).send(result.payload)
+    return
+  }
+
+  return {message: "you've logged in!"}
 }
 
-export const validateStatically = (request: FastifyRequest<{Querystring: IQuerystring}>) => {
-  const { username, password } = request.query;
+export const validateStatically = (request: FastifyRequest<{ Querystring: IQuerystring }>) => {
+  const {username, password} = request.query;
   if (username !== 'admin') {
-    throw new Error("Must be admin")
+    return {statusCode: 400, payload: {message: "Must be admin"}}
   }
   if (password !== 'password') {
-    throw new Error("Wrong password")
+    return {statusCode: 400, payload: {message: "Invalid password"}}
   }
+  return {noProblem: true} as const
 }
 
 interface IQuerystring {
