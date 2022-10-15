66090754eac7fa2908362362e5c06a48a568a719
sumiren <oota.software@gmail.com>
2022-10-15 17:51:55 +0900

    test: add test

--- test/api/good.test.ts
+++ test/api/good.test.ts
@@ -19,3 +19,37 @@ test('good', async() => {
   })
 })
 
+test('good username invalid', async() => {
+  const server = fastify()
+  await server.register(loginHandler)
+
+  const response = await server.inject({
+    method: 'GET',
+    url: '/login?username=admin2&password=password'
+  })
+
+  expect(response.statusCode).toBe(400)
+  expect(response.headers["content-type"]).contains("application/json")
+  expect(JSON.parse(response.body)).toStrictEqual({
+    message: "Must be admin"
+  })
+})
+
+test('good password invalid', async() => {
+  const server = fastify()
+  await server.register(loginHandler)
+
+  const response = await server.inject({
+    method: 'GET',
+    url: '/login?username=admin&password=password2'
+  })
+
+  expect(response.statusCode).toBe(400)
+  expect(response.headers["content-type"]).contains("application/json")
+  expect(JSON.parse(response.body)).toStrictEqual({
+    message: "Invalid password"
+  })
+})
+
+
+
